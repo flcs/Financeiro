acc6b6c70797bdf61aa06a006327b9229db29828
Priscila Manhaes <psilva@iff.edu.br>
2012-02-16 19:44:54 -0200

    Script para instalação de dependencias

---
+++ install.sh
@@ -0,0 +1,7 @@
+#instalação do modulo financeiro
+#nao deve ser chamado utilizando sudo
+
+sudo apt-get install python-dev libxml2
+sudo apt-get install python-setuptools
+easy_install pip
+pip install django==1.3.1
