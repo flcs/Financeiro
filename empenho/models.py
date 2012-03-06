f2a75b914c8dc99dc66d8fd1fa6f10dfd07a888c
Priscila Manhaes <psilva@iff.edu.br>
2012-03-06 11:35:36 -0300

    Criada classes envolvidas em empenho

--- empenho/models.py
+++ empenho/models.py
@@ -4,8 +4,16 @@ from django.db import models
 class Setor(models.Model):
     nome = models.CharField(max_length=20)
 
+
 class Pedido(models.Model):
     coordenacao = models.ForeignKey(Setor)
+    descricao = models.CharField(max_length=200)
+
+
+class Orcamento(models.Model):
+    pedido = models.ForeignKey(Pedido)
+    valor_total = models.DecimalField(max_digits=10, decimal_places=2)
 
 class Empenho(models.Model):
-    pedido = models.ForeignKey(Pedido)
+    orcamento = models.ForeignKey(Orcamento)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
