2d5690228aab7a0ab90725b96ae4821dc8eb3e32
Priscila Manhaes <psilva@iff.edu.br>
2012-03-09 19:01:13 -0300

    Modificado api para possibilitar adicionar pedidos e orcamentos

--- api/handlers.py
+++ api/handlers.py
@@ -1,5 +1,37 @@
 from piston.handler import BaseHandler
-from financeiro.empenho.models import Empenho
+from financeiro.empenho.models import Setor, Pedido, Orcamento, Empenho
+
+
+class PedidoHandler(BaseHandler):
+
+    allowed_methods = ("POST")
+    model = Pedido
+
+    def create(self, request, *args, **kwargs):
+        coordenacao = Setor.objects.get(pk=request.POST.get('coordenacao'))
+        novo_pedido = self.model(coordenacao=coordenacao, descricao=request.POST.get('descricao'))
+        novo_pedido.save()
+        return "Criado\n"
+
+class OrcamentoHandler(BaseHandler):
+
+    allowed_methods = ("POST")
+    model = Orcamento
+
+    def create(self, request, *args, **kwargs):
+        pedido = Pedido.objects.get(pk=request.POST.get('pedido'))
+        novo_orcamento = self.model(pedido=pedido, valor_total=request.POST.get('valor_total'))
+        novo_orcamento.save()
+        return "Criado\n"
+
 
 class EmpenhoHandler(BaseHandler):
+
+    allowed_methods = ("POST")
     model = Empenho
+
+    def create(self, request, *args, **kwargs):
+        orcamento = Orcamento.objects.get(pk=request.POST.get('orcamento'))
+        novo_empenho = self.model(orcamento=orcamento, valor=request.POST.get('valor_total'))
+        novo_empenho.save()
+        return "Criado\n"
--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
 from piston.resource import Resource
-from financeiro.api.handlers import EmpenhoHandler
+from financeiro.api.handlers import PedidoHandler, OrcamentoHandler, EmpenhoHandler
 
+pedido_resource = Resource(PedidoHandler)
+orcamento_resource = Resource(OrcamentoHandler)
 empenho_resource = Resource(EmpenhoHandler)
 
 urlpatterns = patterns('',
-   url(r'^empenho/(?P<id>\d+)$', empenho_resource),
-   url(r'^empenho$', empenho_resource),
+   url(r'^pedidos$', pedido_resource),
+   url(r'^orcamento$', orcamento_resource),
+   url(r'^empenhos$', empenho_resource),
 )
