6709b70d296a0a085e0560541d8b4d236bad1268
Priscila Manhaes <psilva@iff.edu.br>
2012-03-09 19:58:41 -0300

    permitindo acesso a setor

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 from piston.resource import Resource
-from financeiro.api.handlers import PedidoHandler, OrcamentoHandler, EmpenhoHandler
+from financeiro.api.handlers import SetorHandler, PedidoHandler, OrcamentoHandler, EmpenhoHandler
 
+setor_resource = Resource(SetorHandler)
 pedido_resource = Resource(PedidoHandler)
 orcamento_resource = Resource(OrcamentoHandler)
 empenho_resource = Resource(EmpenhoHandler)
@@ -9,6 +10,6 @@ empenho_resource = Resource(EmpenhoHandler)
 urlpatterns = patterns('',
    url(r'^pedidos$', pedido_resource),
    url(r'^orcamentos$', orcamento_resource),
-   url(r'^orcamento$', orcamento_resource),
    url(r'^empenhos$', empenho_resource),
+   url(r'^setores$', setor_resource),
 )
