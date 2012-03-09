0407b591e3d075d28fee0f34e153e6cd5884116f
Priscila Manhaes <psilva@iff.edu.br>
2012-03-09 20:28:23 -0300

    Adicionado manual na pagina inicial para auxilio ao uso

--- urls.py
+++ urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
 from django.contrib import admin
 from django.conf import settings
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', direct_to_template, {'template': 'index.html'}),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('financeiro.api.urls')),
 )
